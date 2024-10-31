da824441e0d12718e0723394c1a8cce4ee949a03
NewajShovo <leoshovo@gmail.com>
2024-10-31 13:01:14 +0600

    Adding stateless fucntion

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
-class NavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav class="navbar bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar
-            <badge className="badge-pill badge-secondary m-2">
-              {this.props.totalCounters}
-            </badge>
-          </a>
-        </div>
-      </nav>
-    );
-  }
-}
+
+const NavBar = (props) => {
+  return (
+    <nav class="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+          <badge className="badge-pill badge-secondary m-2">
+            {props.totalCounters}
+          </badge>
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
