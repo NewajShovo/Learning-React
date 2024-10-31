f91ff975627b831829eef04aa2d2172a57990017
NewajShovo <leoshovo@gmail.com>
2024-10-31 12:03:27 +0600

    Adding Navbar to the project.

--- src/index.js
+++ src/index.js
@@ -7,11 +7,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import Counters from "./components/counters";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Counters />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // ReactDOM.render(
 //   <Counter />,
