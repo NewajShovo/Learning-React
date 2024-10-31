a01166be5de12336169b089433715bc38871dae0
NewajShovo <leoshovo@gmail.com>
2024-10-31 09:33:43 +0600

    Passing value from component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: [],
   };
   handleIncrement = (product) => {
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.count + 1 });
   };
 
   render() {
@@ -25,12 +25,12 @@ class Counter extends Component {
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    return this.state.count === 0 ? "zero" : this.state.count;
+    return this.state.value === 0 ? "zero" : this.state.value;
   }
 }
 
